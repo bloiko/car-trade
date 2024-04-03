ce37e0f67d054e1a733282b1a97b746ba8c192b8
bohdan.loiko <bohdan.loiko@gmail.com>
2024-04-03 21:40:18 +0300

    initial commit + ES setup

---
+++ src/test/java/org/car/trade/CarTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.car.trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarTradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
